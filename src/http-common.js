83858926e5ccf7196751840257e5cde7ae6e5d51
osama806 <osama20033210@gmail.com>
2024-04-29 21:40:31 +0300

    Update headers attribute in http-common file

--- src/http-common.js
+++ src/http-common.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
-
-axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*'
-
 export default axios.create({
   baseURL: "https://osama1410.infinityfreeapp.com/api",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
   }
 })
