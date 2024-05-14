2a5a654ea7c35e793089b3e8b46db247d83c836f
osama806 <osama20033210@gmail.com>
2024-05-14 10:07:38 +0300

    Add publicPath to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/Rental-app-vue/" : "/",
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
