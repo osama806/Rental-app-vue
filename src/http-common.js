932480aa10363eadef538de36c273ddb923e3610
Osama Naser <103202654+osama806@users.noreply.github.com>
2024-05-18 12:13:31 +0300

    Update headers

--- src/http-common.js
+++ src/http-common.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://osama1410.infinityfreeapp.com/api",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "https://osama1410.infinityfreeapp.com, https://estate-rental-app.netlify.app",
   },
 })
