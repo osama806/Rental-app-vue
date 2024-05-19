1d947b8f9204d080b06ceb5658b38381ce9cd555
Osama Naser <103202654+osama806@users.noreply.github.com>
2024-05-19 21:27:18 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  devServer:{
-    proxy: {
-      '^/users': {
-        target: 'https://osama1410.infinityfreeapp.com',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  },
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
